dd161bd2cb1d3b1a5fcaebd1aebd0ec88b7a0af8
VSRDVV <74640120+VSRDVV@users.noreply.github.com>
2021-01-11 16:39:01 +0700

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ request.open('GET','https://restcountries.eu/rest/v2/all',true);
 request.send();
 request.onload = function(){
     var countrydata = JSON.parse(this.response);
-    console.log(countrydata);
+   // console.log(countrydata);
     for(var i =0;i<countrydata.length;i++)
     {
         console.log(countrydata[i].name);
